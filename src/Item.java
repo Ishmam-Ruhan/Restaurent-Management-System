e01523cefed8fdf4afc58990dbe5b8bb78228f61
Ishmam-Ruhan <ishmam15-10769@diu.edu.bd>
2021-10-05 17:49:38 +0600

    Restaurent Management

---
+++ src/Item.java
@@ -0,0 +1,38 @@
+
+public class Item {
+    String fd1,fd2,fd3,fd4,fd5,fd6,fd7,dr1,dr2,dr3,dr4,dr5;
+    String fdamnt1,fdamnt2,fdamnt3,fdamnt4,fdamnt5,fdamnt6,fdamnt7,dramnt1,dramnt2,dramnt3,dramnt4,dramnt5;
+    int token;
+    
+    Item(int token,String fd1,String fdamnt1,String fd2,String fdamnt2,String fd3,String fdamnt3,String fd4,String fdamnt4,String fd5,String fdamnt5,String fd6,String fdamnt6,String fd7,String fdamnt7,String dr1,String dramnt1,String dr2,String dramnt2,String dr3,String dramnt3,String dr4,String dramnt4,String dr5,String dramnt5){
+        this.fd1=fd1;
+        this.fd2=fd2;
+        this.fd3=fd3;
+        this.fd4=fd4;
+        this.fd5=fd5;
+        this.fd6=fd6;
+        this.fd7=fd7;
+        
+        this.dr1=dr1;
+        this.dr2=dr2;
+        this.dr3=dr3;
+        this.dr4=dr4;
+        this.dr5=dr5;
+        
+        this.fdamnt1=fdamnt1;
+        this.fdamnt2=fdamnt2;
+        this.fdamnt3=fdamnt3;
+        this.fdamnt4=fdamnt4;
+        this.fdamnt5=fdamnt5;
+        this.fdamnt6=fdamnt6;
+        this.fdamnt7=fdamnt7;
+        
+        this.dramnt1=dramnt1;
+        this.dramnt2=dramnt2;
+        this.dramnt3=dramnt3;
+        this.dramnt4=dramnt4;
+        this.dramnt5=dramnt5;
+        this.token=token;
+    }
+    
+}
